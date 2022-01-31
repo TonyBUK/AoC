a5ecb8186cc456e5b2092cdd579a9f527cb3f12c
TonyBUK <98747135+TonyBUK@users.noreply.github.com>
2022-01-31 14:08:09 +0000

    Create solver.py

---
+++ 2015/Day04/python/solver.py
@@ -0,0 +1,47 @@
+import time
+import hashlib
+
+def main() :
+
+    # Read the Input File
+    keys = []
+    with open("../input.txt", "r") as inputFile:
+        keys = [line.strip() for line in inputFile.readlines()]
+    #end
+
+    for key in keys :
+
+        nPart1 = -1
+        nPart2 = -1
+        nCount = 1
+        while nPart1 == -1 or nPart2 == -1 :
+
+            hash = hashlib.md5((key + str(nCount)).encode()).hexdigest()
+
+            if nPart1 == -1 :
+                if hash[0:5] == "00000" :
+                    nPart1 = nCount
+                #end
+            #end
+
+            if nPart2 == -1 :
+                if hash[0:6] == "000000" :
+                    nPart2 = nCount
+                #end
+            #end
+
+            nCount += 1
+        #end
+
+        print(f"Part 1: {nPart1}")
+        print(f"Part 2: {nPart2}")
+
+    #end
+
+#end
+
+if __name__ == "__main__" :
+    startTime      = time.perf_counter()
+    main()
+    print(f"{(time.perf_counter() - startTime)}s")
+#end
