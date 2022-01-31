17b4aadd88ed7958e06e4310205b3cbb3417369e
TonyBUK <98747135+TonyBUK@users.noreply.github.com>
2022-01-31 12:14:41 +0000

    Create solver.py

---
+++ 2015/Day01/python/solver.py
@@ -0,0 +1,38 @@
+import time
+
+def main() :
+
+    # Read the Input File
+    lines = []
+    with open("../input.txt", "r") as inputFile:
+        lines = inputFile.readlines()
+    #end
+
+    # Iterate over the Puzzle Input
+    # Note: This should just be one line...
+    assert(len(lines) == 1)
+    for line in lines :
+
+        # Part 1: Subtract the number of ")" from the number of "(" to get the final floor
+        print(f"Part 1: {line.count('(') - line.count(')')}")
+
+        # Part 2: Manually apply the algorithm anf print the index value
+        # Note: The index value is to be output as 1 based, enumerate provides a 0 based index.
+        count = 0
+        for i,c in enumerate(line) :
+            if   c == "(" : count += 1
+            elif c == ")" : count -= 1
+            if count == -1 :
+                print(f"Part 2: {i+1}")
+                break
+            #end
+        #end
+    #end
+
+#end
+
+if __name__ == "__main__" :
+    startTime      = time.perf_counter()
+    main()
+    print(f"{(time.perf_counter() - startTime)}s")
+#end
