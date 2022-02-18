5c7cc0e0cdf2e9710c15e865ceecfaa5a84504e8
Tony B <98747135+TonyBUK@users.noreply.github.com>
2022-02-18 15:15:44 +0000

    Create solver.py

---
+++ 2015/Day17/python/solver.py
@@ -0,0 +1,54 @@
+import time
+
+def main() :
+
+    # Read/Clean the Input File to extract the Buckets
+    containers = []
+    with open("../input.txt", "r") as inputFile:
+        containers = [int(line) for line in inputFile.readlines()]
+    #end
+
+    def calculatePermutations(containers, target, current = 0, count = 0, index = 0, permutations = None) :
+
+        # Handle the first time through...
+        if permutations is None :
+            permutations = {}
+        #end
+
+        # Update the Permutation if we've reached our target
+        if target == current :
+
+            if count not in permutations :
+                permutations[count] = 0
+            #end
+            permutations[count] += 1
+
+            return permutations
+
+        #end
+
+        # Iterate through the remaining possible permutations
+        for i in range(index, len(containers)) :
+            if current + containers[i] <= target :
+                permutations = calculatePermutations(containers, target, current + containers[i], count + 1, i+1, permutations)
+            #end
+        #end
+
+        return permutations
+
+    #end
+
+    permutations = calculatePermutations(containers, 150)
+    total        = sum([i for i in permutations.values()])
+    minSolution  = min(permutations.keys())
+
+    print(f"Part 1: {total}")
+    print(f"Part 2: {permutations[minSolution]}")
+
+#end
+
+if __name__ == "__main__" :
+    startTime      = time.perf_counter()
+    main()
+    print(f"{(time.perf_counter() - startTime)}s")
+#end
