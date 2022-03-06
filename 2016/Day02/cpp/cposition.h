6bd319b513affd3f970e1780d7cba5c35ff4903c
Tony B <tony.r.bell@icloud.com>
2022-03-06 19:55:14 +0000

    Cleaned up alignment.

--- 2016/Day02/cpp/cposition.h
+++ 2016/Day02/cpp/cposition.h
@@ -14,30 +14,30 @@ class CPosition
             int64_t posY;
         };
 
-                    CPosition   ();
-                    CPosition   (const int64_t x, const int64_t y);
-                    CPosition   (const PointType& pos);
-        void              setPos      (const int64_t x, const int64_t y);
-        void              setPos      (const PointType& pos);
-        const PointType&  getPos      () const;
+                            CPosition           ();
+                            CPosition           (const int64_t x, const int64_t y);
+                            CPosition           (const PointType& pos);
+        void                setPos              (const int64_t x, const int64_t y);
+        void                setPos              (const PointType& pos);
+        const PointType&    getPos              () const;
 
-        void              clamp       (const int64_t left, const int64_t top, const int64_t right, const int64_t bottom);
+        void                clamp               (const int64_t left, const int64_t top, const int64_t right, const int64_t bottom);
 
-        CPosition   operator+   (const CPosition& pos) const;
-        CPosition   operator+   (const int64_t    n) const;
+        CPosition           operator+           (const CPosition& pos) const;
+        CPosition           operator+           (const int64_t    n) const;
 
-        CPosition   operator*   (const CPosition& pos) const;
-        CPosition   operator*   (const int64_t    n) const;
+        CPosition           operator*           (const CPosition& pos) const;
+        CPosition           operator*           (const int64_t    n) const;
 
-        CPosition&  operator+=  (const CPosition& pos);
-        CPosition&  operator+=  (const int64_t    n);
+        CPosition&          operator+=          (const CPosition& pos);
+        CPosition&          operator+=          (const int64_t    n);
 
-        CPosition&  operator*=  (const CPosition& pos);
-        CPosition&  operator*=  (const int64_t    n);
+        CPosition&          operator*=          (const CPosition& pos);
+        CPosition&          operator*=          (const int64_t    n);
 
-        bool        operator<   (const CPosition& pos) const;
+        bool                operator<           (const CPosition& pos) const;
 
-        std::size_t manhattenDistance() const;
+        std::size_t         manhattenDistance   () const;
 
     private:
 
