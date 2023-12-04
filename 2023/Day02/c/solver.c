7ebbd583a7e630d6067d1c68b40db89097aef0bb
TonyBUK <megatony@gmail.com>
2023-12-04 22:49:29 +0000

    Removed heap usage.

--- 2023/Day02/c/solver.c
+++ 2023/Day02/c/solver.c
@@ -3,35 +3,8 @@
 #include <string.h>
 #include <assert.h>
 
-#define GAME_STRING        ("Game ")
-#define GAME_STRING_LENGTH (sizeof(GAME_STRING) - 1)
-
-unsigned long getMaxColour(const char* kColour, const char* kLine)
-{
-    unsigned long nMaxColour = 0;
-    char*         pBuffer;
-
-    pBuffer = strstr(kLine, kColour);
-    while (pBuffer)
-    {
-        /* The Digit always precedes this... */
-        char* pLocalBuffer = pBuffer - 1;
-        unsigned long nColourCount;
-        while (*pLocalBuffer != ' ')
-        {
-            --pLocalBuffer;
-        }
-        nColourCount = strtoul(pLocalBuffer + 1, NULL, 10);
-        if (nColourCount > nMaxColour)
-        {
-            nMaxColour = nColourCount;
-        }
-
-        pBuffer = strstr(pBuffer+1, kColour);
-    }
-
-    return nMaxColour;
-}
+#define AOC_FALSE (0u)
+#define AOC_TRUE  (1u)
 
 int main(int argc, char** argv)
 {
@@ -39,32 +12,104 @@ int main(int argc, char** argv)
  
     if (pData)
     {
-        size_t        nBufferLength = 0;
-        char*         kLine         = NULL;
-
         unsigned long nPartOne      = 0u;
         unsigned long nPartTwo      = 0u;
 
         /* Extract the Max Red/Green/Blue per Game */
-        while (-1 != getline(&kLine, &nBufferLength, pData))
+        while (!feof(pData))
         {
-            const unsigned long nGameId = strtoul(strstr(kLine, GAME_STRING) + GAME_STRING_LENGTH, NULL, 10);
+            unsigned      bGameIdFound      = AOC_FALSE;
+            unsigned      bSpaceFound       = AOC_FALSE;
+            unsigned      bProcessingNumber = AOC_FALSE;
+            unsigned      bCubeIdentified   = AOC_FALSE;
 
-            /* Max Colours */
-            const unsigned long nRed    = getMaxColour(" red",   kLine);
-            const unsigned long nGreen  = getMaxColour(" green", kLine);
-            const unsigned long nBlue   = getMaxColour(" blue",  kLine);
+            unsigned long nGameId           = 0u;
+            unsigned long nCubeCount        = 0u;
+
+            char          kData;
+            unsigned long nMaxRed           = 0u;
+            unsigned long nMaxGreen         = 0u;
+            unsigned long nMaxBlue          = 0u;
+
+            while (('\n' != (kData = fgetc(pData))) && (!feof(pData)))
+            {
+                /* First find the Game Id */
+                if (AOC_FALSE == bGameIdFound)
+                {
+                    if (AOC_FALSE == bSpaceFound)
+                    {
+                        if (' ' == kData)
+                        {
+                            bSpaceFound = AOC_TRUE;
+                        }
+                    }
+                    else if (':' == kData)
+                    {
+                        bGameIdFound      = AOC_TRUE;
+                        bSpaceFound       = AOC_FALSE;
+                        bProcessingNumber = AOC_FALSE;
+                        bCubeIdentified   = AOC_FALSE;
+                        nCubeCount        = 0u;
+                    }
+                    else
+                    {
+                        assert((kData >= '0') && (kData <= '9'));
+                        nGameId = (nGameId * 10) + (kData - '0');
+                    }
+                }
+                else
+                {
+                    /* Then find the Cube Amounts as pairs of data split by spaces, Number, then Colour. */
+                    if (' ' == kData)
+                    {
+                        if (AOC_FALSE == bProcessingNumber)
+                        {
+                            bProcessingNumber = AOC_TRUE;
+                            bCubeIdentified   = AOC_FALSE;
+                        }
+                        else
+                        {
+                            bProcessingNumber = AOC_FALSE;
+                        }
+                    }
+                    else if (AOC_TRUE == bProcessingNumber)
+                    {
+                        assert((kData >= '0') && (kData <= '9'));
+                        nCubeCount = (nCubeCount * 10) + (kData - '0');
+                    }
+                    else if (AOC_FALSE == bCubeIdentified)
+                    {
+                        if ('r' == kData)
+                        {
+                            bCubeIdentified = AOC_TRUE;
+                            if (nCubeCount > nMaxRed) nMaxRed = nCubeCount;
+                            nCubeCount = 0u;
+                        }
+                        else if ('g' == kData)
+                        {
+                            bCubeIdentified = AOC_TRUE;
+                            if (nCubeCount > nMaxGreen) nMaxGreen = nCubeCount;
+                            nCubeCount = 0u;
+                        }
+                        else if ('b' == kData)
+                        {
+                            bCubeIdentified = AOC_TRUE;
+                            if (nCubeCount > nMaxBlue) nMaxBlue = nCubeCount;
+                            nCubeCount = 0u;
+                        }
+                    }
+                }
+            }
 
             /* Part 1 */
-            if ((nRed <= 12) && (nGreen <= 13) && (nBlue <= 14))
+            if ((nMaxRed <= 12) && (nMaxGreen <= 13) && (nMaxBlue <= 14))
             {
                 nPartOne += nGameId;
             }
 
             /* Part 2 */
-            nPartTwo += nRed * nGreen * nBlue;
+            nPartTwo += nMaxRed * nMaxGreen * nMaxBlue;
         }
-        free(kLine);
 
         fclose(pData);
 
